79e0083bbdc305384c12d77b0389f22031db6ee9
Isen-kun <60184753+Isen-kun@users.noreply.github.com>
2023-04-28 16:07:17 +0530

    28/4/2023

---
+++ Back End/ADO_Example/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(localdb)\ProjectModels;Initial Catalog=CognizantINTIOT23DFSE001");
+            string strSelectAllQuery = "select * from Employee";
+            SelectEmployee(con, strSelectAllQuery);
+            Console.ReadLine();
+        }
+
+        public static void SelectEmployee(SqlConnection con, string strSelectQuery)
+        {
+            SqlCommand com = new SqlCommand(strSelectQuery, con);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Employee");
+            if (ds != null && ds.Tables.Count > 0 && ds.Tables["Employee"].Rows.Count > 0)
+            {
+                Console.WriteLine("====================Employee information=====================");
+                foreach (DataRow row in ds.Tables["Employee"].Rows)
+                {
+                    var EmpId = row["EmpId"];
+                    var EmpName = row["EmpName"];
+                    var EmpAddress = row["EmpAddress"];
+                    var EmpCountry = row["EmpCountry"];
+                    var EmpEmail = row["EmpEmail"];
+                    Console.WriteLine("==========" + EmpName + "==========");
+                    Console.WriteLine($"EmpId:{EmpId}  EmpName:{EmpName}  EmpAddress:{EmpAddress}  EmpCountry:{EmpCountry} EmpEmail:{EmpEmail}");
+                }
+            }
+        }
+    }
+}
