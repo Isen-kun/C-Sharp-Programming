18d828d3d0a5f4aba1c9417cf36de0ad2c462318
Isen-kun <60184753+Isen-kun@users.noreply.github.com>
2023-05-05 09:25:19 +0530

    Entity practice

---
+++ Back End/EntityDBFirstDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityDBFirstDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DemoDBEntities();
+            var emp = new Employee()
+            {
+                EmpId = 4,
+                EmpName = "John Cena",
+                EmpAddress = "Ohio",
+                EmpCountry = "US",
+                EmpEmail = "John.cena@gmail.com"
+            };
+            context.Employees.Add(emp);
+            context.SaveChanges();
+        }
+    }
+}
