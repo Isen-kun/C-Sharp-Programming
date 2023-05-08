f6a241f1465dbbee4be0a1987c2e3e60df3a0b61
Isen-kun <60184753+Isen-kun@users.noreply.github.com>
2023-05-08 17:55:03 +0530

    Demo REST API using ASP.Net Core WebAPI

---
+++ Back End/DemoRestAPI/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstateAPI.Models;
+
+namespace RealEstateAPI.Data
+{
+    public class ApiDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RealEstateDb");
+        }
+    }
+}
---
+++ Back End/DemoRestAPI/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstateAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Category name can't be null or empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Category image URL can't be null or empty")]
+        public string ImageUrl { get; set; }
+
+        //public string Description { get; set; }
+        //deleted column
+    }
+}
